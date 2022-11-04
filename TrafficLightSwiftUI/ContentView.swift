3db922a42a250536a34ce7521a5d276f4bce105e
Olga Yurchuk <olga.yourchooke@gmail.com>
2022-11-04 17:38:46 +0700

    All logic is implemented

--- TrafficLightSwiftUI/ContentView.swift
+++ TrafficLightSwiftUI/ContentView.swift
@@ -8,9 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var buttonText = "Start"
+    @State private var redOpacity = 0.5
+    @State private var yellowOpacity = 0.5
+    @State private var greenOpacity = 0.5
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color(.lightGray)
+                .ignoresSafeArea()
+            VStack {
+                circleLight(color: .red)
+                    .opacity(redOpacity)
+                circleLight(color: .yellow)
+                    .opacity(yellowOpacity)
+                circleLight(color: .green)
+                    .opacity(greenOpacity)
+                
+                Spacer()
+                
+                Button(action: {
+                    buttonText = "Next"
+                    changeLight()
+                }) {
+                    Text(buttonText)
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                }
+                    .buttonStyle(.borderedProminent)
+                .padding()
+            }
+        }
+    }
+    
+    private func changeLight() {
+        if redOpacity == 0.5 && yellowOpacity == 0.5 && greenOpacity == 0.5 {
+            redOpacity = 1.0
+        } else if redOpacity == 1.0 {
+            redOpacity = 0.5
+            yellowOpacity = 1.0
+            greenOpacity = 0.5
+        } else if yellowOpacity == 1.0 {
+            yellowOpacity = 0.5
+            greenOpacity = 1.0
+        } else if greenOpacity == 1.0 {
+            greenOpacity = 0.5
+            redOpacity = 1.0
+        }
+        
     }
 }
 
---
+++ TrafficLightSwiftUI/circleLight.swift
@@ -0,0 +1,26 @@
+//
+//  circleLight.swift
+//  TrafficLightSwiftUI
+//
+//  Created by Olga Yurchuk on 04.11.2022.
+//
+
+import SwiftUI
+
+struct circleLight: View {
+    @State var color:  Color
+    
+    var body: some View {
+        Circle()
+            .frame(width: 150, height: 150)
+            .foregroundColor(color)
+            .overlay(Circle().stroke(Color.white, lineWidth: 5))
+            .shadow(radius: 10)
+    }
+}
+
+struct circleLight_Previews: PreviewProvider {
+    static var previews: some View {
+        circleLight(color: .red)
+    }
+}
